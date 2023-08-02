45082507eb21e122042dff1bf0e5213f2243839c
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-08-02 15:24:07 +0200

    Delete ProgettoMO directory

--- ProgettoMO/src/main/java/Main.java
+++
@@ -1,8 +0,0 @@
-import it.units.inginf.mathematicaloptimization.Alns;
-
-public class Main {
-    public static void main(String[] args) {
-        Alns alns = new Alns();
-        int[][] x = alns.run();
-    }
-}
