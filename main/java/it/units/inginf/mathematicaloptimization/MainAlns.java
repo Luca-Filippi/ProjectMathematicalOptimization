42ced39227a3eb39cbc8d00fc39bd90aeac1ed89
Luca-Filippi <96942335+Luca-Filippi@users.noreply.github.com>
2023-08-07 16:24:45 +0200

    Delete main/java/it/units/inginf/mathematicaloptimization directory

--- main/java/it/units/inginf/mathematicaloptimization/MainAlns.java
+++
@@ -1,9 +0,0 @@
-package it.units.inginf.mathematicaloptimization;
-
-public class MainAlns {
-    public static void main(String[] args) {
-        System.out.println("Comincio a calcolare la soluzione di ALNS");
-        Alns alns = new Alns();
-        int[][] x = alns.run();
-    }
-}
--- main/java/it/units/inginf/mathematicaloptimization/MainDalns.java
+++
@@ -1,9 +0,0 @@
-package it.units.inginf.mathematicaloptimization;
-
-public class MainDalns {
-    public static void main(String[] args) {
-        System.out.println("Comincio a calcolare la soluzione di DALNS");
-        Dalns dalns = new Dalns();
-        int[][] x = dalns.run();
-    }
-}
